214e2e1c83da84af069ba06c57501c03065c0f8b
Ramon Hubbell <Ramon.hubbell@gmail.com>
2021-01-18 13:45:17 -0600

    feature: created recipe model and working on displaying on template

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes : Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 
+    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.recipetineats.com%2Fhoney-garlic-chicken%2F&psig=AOvVaw2srC6Ir63Wr_4AyQqrnsCO&ust=1611085302288000&source=images&cd=vfe&ved=2ahUKEwj83LfKnqbuAhUWg6wKHb3BAcoQjRx6BAgAEAc')
+  ];
 
   constructor() { }
 
